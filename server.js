ac55c6ad3b9ae8b2f3b1f125320e42b9edc39295
YuriiLitovinskyi <y.litovinskyi@gmail.com>
2020-04-17 23:46:21 +0300

    version 1.0.0

---
+++ server.js
@@ -0,0 +1,65 @@
+const MongoClient = require('mongodb').MongoClient;
+const prompts = require('prompts');
+
+const url = 'mongodb://localhost:27017/DBClientsPPK';
+
+(() => {
+    MongoClient.connect(url, async (err, db) => {
+        if(err) {            
+            console.log('No connection to Database! Please start MongoDB service on default port 27017!\n');                       
+            
+            console.log(err);
+            await sleep(10000);           
+        } else {
+            console.log('Connected to database successfully!\n'); 
+
+            (async () => {            
+                const response = await prompts({
+                    type: 'number',
+                    name: 'value',
+                    message: 'Enter ppk number to delete: ',
+                    //validate: value => value <= 0 || value > 1000 ? `Please enter a valid number from 1 to 1000` : true
+                  });
+    
+                  deletePpk(db, response.value);
+            })();
+        };       
+    });
+})();
+
+
+const sleep = (timeout) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, timeout);        
+    });
+};
+
+const deletePpk = (db, ppkNum) => {
+    db.collection('ppkState', async (err, collection) => {
+        if(err) {
+            console.log(err);
+            await sleep(15000);
+        };
+
+        const ppkCount = await collection.find({ ppk_num: ppkNum }).count();
+        
+        if (ppkCount === 0){
+            console.log(`Ppk number ${ppkNum} wasn't found in database... `);
+            db.close();
+            await sleep(5000);
+        } else {
+            collection.remove({ ppk_num: ppkNum }, async (err, result) => {
+                if(err) {
+                    console.log(err);
+                    await sleep(15000);
+                };
+    
+                console.log(`ppk ${ppkNum} was removed from database successfully: ${result}`);
+                db.close();
+    
+                await sleep(5000);
+            });
+        };      
+    });
+};
+
